ffd187a34c8ebb7b97734641f20b3d5fb283c348
praveen <praveendavin@gmail.com>
2025-01-08 20:32:19 +0530

    30% completed added maps and more

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import './App.css'
 import hiMemoji from './assets/hi.png'
 import { Left } from './components/left'
+import { MyLocation } from './components/my-location';
 
 function App() {
 
@@ -8,12 +9,20 @@ function App() {
     <div className='flex gap-4 p-4 bg-black dark:bg-white dark:text-black text-white h-[100dvh]'>
       <div className='flex flex-col gap-4 w-[50%] h-full'>
         <div className='flex gap-4 h-[50%]'>
-          <div className='bg-[#1d1d1d] dark:bg-[#e5e5e5] w-[50%] rounded-lg'>Left-two</div>
-          <div className='bg-[#1d1d1d] dark:bg-[#e5e5e5] w-[50%] rounded-lg'>Left-three</div>
+          <div className='bg-[#1d1d1d] flex flex-col gap-4 justify-center p-4 text-[18px] dark:bg-[#e5e5e5] w-[50%] rounded-lg'>
+          <div className='font-semibold text-[5rem]'>About Me</div> 
+          Passionate full-stack developer with 4 years of experience crafting impactful web solutions. I thrive on solving complex problems, creating seamless user experiences, and continuously learning to push the boundaries of what’s possible in software development. Dedicated to delivering high-quality, efficient, and innovative solutions that make a difference.
+          </div>
+          <div className='bg-[#1d1d1d] dark:bg-[#e5e5e5] w-[50%] rounded-lg'>
+            <MyLocation/>
+          </div>
         </div>
-        <div className='w-full flex flex-col justify-center p-4 bg-[#1d1d1d] dark:bg-[#e5e5e5] h-[50%] rounded-lg'>
-          <div className='text-[7rem] font-bold'>PRAVEEN KUMAR</div>
-          <div className='text-[4rem] font-semibold'>FULLSTACK DEVLOPER</div>
+        <div className='w-full flex items-center p-4 bg-[#1d1d1d] dark:bg-[#e5e5e5] h-[50%] rounded-lg'>
+          <img src={hiMemoji} alt="" className='w-[20rem] h-[20rem]' />
+          <div className='flex flex-col items-center justify-center'>
+          <div className='text-[4rem] font-bold motion-preset-typewriter '>PRAVEEN KUMAR</div>
+          <div className='text-[2rem] font-semibold motion-preset-typewriter '>FULLSTACK DEVLOPER</div>
+          </div>
         </div>
       </div>
       <div className='flex flex-col gap-4 w-[50%] h-full'>
---
+++ src/components/my-location.tsx
@@ -0,0 +1,64 @@
+import { useEffect, useRef } from "react";
+import { Loader } from "@googlemaps/js-api-loader";
+import icon from '../assets/working.png'
+import darkMode from './dark-mode.json'
+
+
+export const MyLocation = () => {
+    const mapRef = useRef(null);
+
+    async function init() {
+        if (mapRef.current) {
+            const loader = new Loader({
+                apiKey: `${import.meta.env.VITE_GOOGLE_API_KEY}`, // Replace with your actual API key
+                version: "beta", // Required for AdvancedMarkerElement
+                libraries: ["marker"], // Load the marker librarym
+            });
+            const res = await loader.importLibrary("core");
+            if (res) {
+                const map = new google.maps.Map(mapRef.current, {
+                    center: { lat: 11.0168, lng: 76.9558 }, // San Francisco coordinates
+                    zoom: 9,
+                    mapId: 'one',
+                    disableDefaultUI:true,
+                    styles:darkMode
+                });
+                const markerElement = document.createElement("div");
+                markerElement.style.display = "flex";
+                markerElement.style.flexDirection = "column";
+                markerElement.style.alignItems = "center";
+                markerElement.style.background = "rgba(0, 0, 0, 0.5)";
+                markerElement.style.border = "3px solid #fff";
+                markerElement.style.borderRadius = "50%";
+
+                const img = document.createElement("img");
+                img.src = icon; // Replace with your image URL
+                img.alt = "Custom Marker";
+                img.style.width = "100px"; // Adjust width
+                img.style.height = "100px"; // Adjust height
+
+                const text = document.createElement("div");
+                text.style.marginTop = "5px";
+                text.style.color = "#333";
+                text.style.fontSize = "14px";
+
+                markerElement.appendChild(img);
+                markerElement.appendChild(text);
+
+                new google.maps.marker.AdvancedMarkerElement({
+                    position: { lat: 11.0168, lng: 76.9558 },
+                    map: map,
+                    content: markerElement,
+                });
+            }
+        }
+    }
+
+    useEffect(() => {
+        init();
+    }, []);
+    console.log(import.meta.env.GOOGLE_API_KEY)
+    return (
+        <div ref={mapRef} style={{ width: "100%", height: "450px", borderRadius:'10px' }} />
+    );
+};
